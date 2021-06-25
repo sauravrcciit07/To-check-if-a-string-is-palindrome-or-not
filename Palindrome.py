37675701b2e050b36ae65fc5949d073104ad951f
sauravrcciit07 <60874490+sauravrcciit07@users.noreply.github.com>
2021-06-25 21:06:56 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,18 @@
+# function to check string is
+# palindrome or not
+def isPalindrome(str):
+ 
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+ 
+# main function
+s = input()
+ans = isPalindrome(s)
+ 
+if (ans):
+    print("It is a Palindrome.")
+else:
+    print("It is not a Palindrome string.")
